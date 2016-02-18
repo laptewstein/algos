8d509d3767a2e325fc80464aa7b0adbb39d3eb84
Vlad Nikiporoff <vladnikiporov@gmail.com>
2016-02-17 22:21:53 -0500

    second-smallest: more  pythonic

--- lists/second_smallest-in-array.py
+++ lists/second_smallest-in-array.py
@@ -5,33 +5,32 @@ Given an array of integers, find the smallest and second smallest element.
 import sys
 import random
 import operator
-from typing import List, Tuple, Union
+from typing import List, Tuple
 
 
 def second_smallest(lst: List[int]) -> Tuple[List, int, int]:
 
     # There should be at least two elements
-    arr_len = len(lst)
-    if operator.lt(arr_len, 2):
-        print("Invalid Input")
-        return None
+    if len(lst) < 2:
+        msg = "Invalid Input"
+        return lst, msg, msg
 
     absolute_low = second_smallest = sys.maxsize
-    for i in range(0, arr_len):
+    for elem in lst:
 
         # current element is lower than lowest?
-        if lst[i] < absolute_low:
+        if elem < absolute_low:
 
             # make (currently) lowest a second_smallest: we found lower
             second_smallest = absolute_low
             # then assign this all-time low as absolute
-            absolute_low = lst[i]
+            absolute_low = elem
 
         # current element in between absolute low and second-smallest?
-        elif absolute_low < lst[i] < second_smallest:
+        elif absolute_low < elem < second_smallest:
 
             # update second_smallest
-            second_smallest = lst[i]
+            second_smallest = elem
 
     if operator.eq(second_smallest, sys.maxsize):
         print("No second smallest element")
