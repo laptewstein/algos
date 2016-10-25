05c2f9a4fcfc61476ccdf84bde96b4db3f83bdd8
Vlad Nikiporoff <vladnikiporov@gmail.com>
2016-10-24 20:14:48 -0400

    sync

--- lists/fibonacci.py
+++ lists/fibonacci.py
@@ -1,14 +1,28 @@
+"""
+Print first n elements from a fibonacci sequence
+"""
 
-def fibonacci():
+
+def fibonacci_gen():
+    """
+    Fibonacci Generator
+    """
     a, b = 0, 1
     while True:
         yield a
         a, b = b, a + b
 
 
-f = fibonacci()
-# print first 20 elements
-print(list(map(
-    next,
-    (f for _ in range(20))
-)))
+def first_n_elements(steps, gen):
+    return map(
+        next,
+        (gen for _ in range(steps))
+    )
+
+
+if __name__ == '__main__':
+    steps = first_n_elements(
+        steps=20,
+        gen=fibonacci_gen()
+    )
+    print(list(steps))
