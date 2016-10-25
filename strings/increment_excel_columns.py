beea4cefd359ff602fb8d91edf35c659b114686b
Vlad Nikiporoff <vladnikiporov@gmail.com>
2016-10-24 22:55:48 -0400

    added additional excel incrementation algo

--- strings/increment_excel_columns.py
+++ strings/increment_excel_columns.py
@@ -2,12 +2,11 @@
 Given sequennce of characters, return next in alphabetic order.
 Space is ignored (discarded); otherwise result of 'Z ZZ' should be defined ('A AAA' or 'AA AA'), which is right?
 """
-
 import string
 def increment_excel(s=''):
-    """
+    '''
     A->B, Z->AA, AZ->BA, KZZ->LAA
-    """
+    '''
     def wrapped(s):
         if not s: 
             return string.uppercase[0]
@@ -18,7 +17,32 @@ def increment_excel(s=''):
     return wrapped(s.upper().replace(' ',''))
 
 
-increment_excel('TRYME') # -> 'TRYMF'
-increment_excel('TRY ZZ') # -> 'TRZAA'
-increment_excel('ABCC') # -> 'ABCD'
-increment_excel('ZZ Z') # -> 'AAAA'
+
+
+def seqconverter(a):
+    def converter(number):
+        retval = str()
+        while number:
+            x = (number - 1) % 26
+            number = (number - 1) / 26
+            retval = '%s%s' % (chr(x + 65), retval)
+        return retval
+
+    def numconverter(a):
+        res = 0
+        for c in a:
+            res *= 26
+            res += ord(c) - 64
+        return res
+    return converter(numconverter(a) + 1)
+
+
+
+seqconverter('ZYZ')     # -> 'ZZA'
+seqconverter('ZZA')     # -> 'ZZB'
+seqconverter('XY')      # -> 'XZ'
+
+seqconverter('TRYME')   # -> 'TRYMF'
+seqconverter('TRYZZ')   # -> 'TRZAA'
+seqconverter('ABCC')    # -> 'ABCD'
+seqconverter('ZZZ')     # -> 'AAAA'
