5ecb3113e48a0a6cd3035194b0cbd527de94c73d
Kartoshka <vladmilshtein@gmail.com>
2021-12-22 02:42:36 -0500

    max-rectangle sync

--- matrix/max_rectangle.rb
+++ matrix/max_rectangle.rb
@@ -22,10 +22,10 @@ class Solution
         # height of the rectangle
         height = array[index_sequence.pop]
 
-        # width of the rectangle from current index to previous index: 
+        # take height of the first element and multiply by count  
         width = index.pred - index_sequence.last #  0 - (-1) => 1
         areas << height * width # optionally: [previous_max_area, curent_max_area].max 
-        puts("[!OMG!] pop #{height} (value of last index ^), width (#{index.pred} (index) - #{index_sequence.last} (index_sequence.last)) index_sequence #{index_sequence}, #{height} * #{width} => #{areas}")
+        puts("[!OMG!] index_sequence.pop of #{height}, width (#{index.pred} (index) - #{index_sequence.last} (index_sequence.last)) index_sequence #{index_sequence}, #{height} * #{width} => #{areas}")
       end  
       index_sequence << index # [-1, index .. current]
       puts("[#{index}] goes into #{index_sequence}")
@@ -36,32 +36,33 @@ end
 
 #####
 puts Solution.largest_rectangle_area([2,1,5,6,2,3,2,3])
+
 # Array: [2, 1, 5, 6, 2, 3, 2, 3] + [0]
 # 
 # [0] 2 is higher than 0 (of index_sequence [-1].last)
 # [0] goes into [-1, 0]
 # [1] 1 is LOWER than 2 (of index_sequence [-1, 0].last)
-# [!OMG!] pop 2 (value of last index ^), width (0 (index) - -1 (index_sequence.last)) index_sequence [-1], 2 * 1 => [2]
+# [!OMG!] index_sequence.pop of 2, width (0 (index) - -1 (index_sequence.last)) index_sequence [-1], 2 * 1 => [2]
 # [1] goes into [-1, 1]
 # [2] 5 is higher than 1 (of index_sequence [-1, 1].last)
 # [2] goes into [-1, 1, 2]
 # [3] 6 is higher than 5 (of index_sequence [-1, 1, 2].last)
 # [3] goes into [-1, 1, 2, 3]
 # [4] 2 is LOWER than 6 (of index_sequence [-1, 1, 2, 3].last)
-# [!OMG!] pop 6 (value of last index ^), width (3 (index) - 2 (index_sequence.last)) index_sequence [-1, 1, 2], 6 * 1 => [2, 6]
-# [!OMG!] pop 5 (value of last index ^), width (3 (index) - 1 (index_sequence.last)) index_sequence [-1, 1], 5 * 2 => [2, 6, 10]
+# [!OMG!] index_sequence.pop of 6, width (3 (index) - 2 (index_sequence.last)) index_sequence [-1, 1, 2], 6 * 1 => [2, 6]
+# [!OMG!] index_sequence.pop of 5, width (3 (index) - 1 (index_sequence.last)) index_sequence [-1, 1], 5 * 2 => [2, 6, 10]
 # [4] goes into [-1, 1, 4]
 # [5] 3 is higher than 2 (of index_sequence [-1, 1, 4].last)
 # [5] goes into [-1, 1, 4, 5]
 # [6] 2 is LOWER than 3 (of index_sequence [-1, 1, 4, 5].last)
-# [!OMG!] pop 3 (value of last index ^), width (5 (index) - 4 (index_sequence.last)) index_sequence [-1, 1, 4], 3 * 1 => [2, 6, 10, 3]
+# [!OMG!] index_sequence.pop of 3, width (5 (index) - 4 (index_sequence.last)) index_sequence [-1, 1, 4], 3 * 1 => [2, 6, 10, 3]
 # [6] goes into [-1, 1, 4, 6]
 # [7] 3 is higher than 2 (of index_sequence [-1, 1, 4, 6].last)
 # [7] goes into [-1, 1, 4, 6, 7]
 # [8] 0 is LOWER than 3 (of index_sequence [-1, 1, 4, 6, 7].last)
-# [!OMG!] pop 3 (value of last index ^), width (7 (index) - 6 (index_sequence.last)) index_sequence [-1, 1, 4, 6], 3 * 1 => [2, 6, 10, 3, 3]
-# [!OMG!] pop 2 (value of last index ^), width (7 (index) - 4 (index_sequence.last)) index_sequence [-1, 1, 4], 2 * 3 => [2, 6, 10, 3, 3, 6]
-# [!OMG!] pop 2 (value of last index ^), width (7 (index) - 1 (index_sequence.last)) index_sequence [-1, 1], 2 * 6 => [2, 6, 10, 3, 3, 6, 12]
-# [!OMG!] pop 1 (value of last index ^), width (7 (index) - -1 (index_sequence.last)) index_sequence [-1], 1 * 8 => [2, 6, 10, 3, 3, 6, 12, 8]
+# [!OMG!] index_sequence.pop of 3, width (7 (index) - 6 (index_sequence.last)) index_sequence [-1, 1, 4, 6], 3 * 1 => [2, 6, 10, 3, 3]
+# [!OMG!] index_sequence.pop of 2, width (7 (index) - 4 (index_sequence.last)) index_sequence [-1, 1, 4], 2 * 3 => [2, 6, 10, 3, 3, 6]
+# [!OMG!] index_sequence.pop of 2, width (7 (index) - 1 (index_sequence.last)) index_sequence [-1, 1], 2 * 6 => [2, 6, 10, 3, 3, 6, 12]
+# [!OMG!] index_sequence.pop of 1, width (7 (index) - -1 (index_sequence.last)) index_sequence [-1], 1 * 8 => [2, 6, 10, 3, 3, 6, 12, 8]
 # [8] goes into [-1, 8]
 # 12
