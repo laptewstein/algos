4a4a4d4c4814bea88adf5d60abccf5c762846c11
vnikiporov <vnikiporov@zynga.com>
2022-02-13 16:08:20 -0500

    car / cdr

--- dynamic_programming/enclosed_itemgetter_methods.py
+++ dynamic_programming/enclosed_itemgetter_methods.py
@@ -1,9 +1,9 @@
 # cons(a, b) constructs a pair,
 # and car(pair) and cdr(pair) returns the first and last element of that pair.
 
-# For example,
+# For example:
 # car(cons(3, 4)) returns 3,
-# and
+# AND
 # cdr(cons(3, 4)) returns 4.
 
 # Given this implementation of cons:
