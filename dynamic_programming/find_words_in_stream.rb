883a1d41c0f1d89b18672734f1eb577107eb9e70
vnikiporov <vnikiporov@zynga.com>
2022-11-23 12:08:11 -0500

    algolia TIr2

---
+++ dynamic_programming/find_words_in_stream.rb
@@ -0,0 +1,47 @@
+# // Input:
+# // 1. magazine to cut out words from as list of words
+# // 2. a message to reconstitute
+# // 
+# // Output: boolean whether it is possible to build the message from the magazine.
+
+magazine = [
+  'Ruby',
+  'environment',
+  'hello',
+  'ready',
+  'Everyone',
+  'can',
+  'toronto',
+  'from',
+  'Paris'
+].map(&:downcase)
+
+message = "Hello from Toronto".downcase.split
+
+# O(number of words)
+def preprocess_string(magazine)
+  hash = {}.tap do |h|
+    magazine.each do |word|
+      h[word] ||= 0          # initialize counter if not found
+      h[word] = h[word].succ # increment count per word
+    end
+  end
+end
+
+# O(num of words in message * number of words in magazine)
+def message_reconstruction_is_possible?(article, message)
+  counters = preprocess_string(article)
+  message.select do |word|
+    count = counters[word]
+    next false unless counters[word]
+    if count.pred == 0
+      counters.delete word
+      next true
+    end
+    counters[word] = count.pred
+    true
+  end.count == message.count
+end
+
+puts message_reconstruction_is_possible?(magazine, message)
+
