35a44d4c0cb05838d1290ca7335262088a307f0e
Vlad Milshtein <kartoshka548@gmail.com>
2022-12-13 01:21:38 -0500

    https://leetcode.com/problems/first-missing-positive/

--- lists/first_missing_positive_from_unsorted_array.rb
+++ lists/first_missing_positive_from_unsorted_array.rb
@@ -3,6 +3,8 @@
 # Given an unsorted integer array nums, return the smallest missing positive integer.
 # You must implement an algorithm that runs in O(n) time and uses constant extra space.
 
+# Time Complexity: O(n), only two traversals are needed.
+# Space complexity: O(1), no extra space is needed
 def first_missing_positive(nums)
   index = 0
   while index < nums.count
@@ -19,31 +21,6 @@ def first_missing_positive(nums)
   end
   return nums.count.succ
 end
-
-# for learning purposes,
-# lets add a few print statements:
-def first_missing_positive(nums)
-  index = 0
-  while index < nums.count
-    current_value = nums[index]
-    proper_index = current_value.pred
-    # puts nums.inspect
-    # puts "index #{index}, current_value: #{current_value}, proper_index #{proper_index}"
-    if current_value > 0 && proper_index < nums.count && current_value != nums[proper_index]
-      nums[proper_index], nums[index] = current_value, nums[proper_index]
-    else
-      # puts "incrementing index, number is either negative or at the right location"
-      index = index.succ
-    end
-  end
-  # puts nums.inspect
-  for index in 0...nums.count
-    return index.succ unless nums[index] == index.succ
-  end
-  return nums.count.succ
-end
-
-puts first_missing_positive [3,4,-1,1]
 
 # [3, 4, -1, 1]
 # index 0, current_value: 3, proper_index 2
@@ -65,4 +42,41 @@ puts first_missing_positive [3,4,-1,1]
 # incrementing index, number is either negative or at the right location
 # [1, -1, 3, 4]
 
+# => 2
+
+# alternative implementation, same idea
+def first_missing_positive(nums)
+  for index in 0...nums.count
+    # puts "index #{index}, nums: #{nums.inspect}"
+    while nums[index] > 0 && nums[index].pred < nums.count && nums[index] != nums[nums[index].pred]
+      nums[nums[index].pred], nums[index] = nums[index], nums[nums[index].pred]
+      # puts ">> nums: #{nums.inspect}"
+    end
+    # puts "incrementing index, number is either negative or at the right location"
+  end
+  # puts nums.inspect
+  for index in 0...nums.count
+    return index.succ unless nums[index] == index.succ
+  end
+  return nums.count.succ
+end
+
+puts first_missing_positive [3, 4, -1, 1]
+
+# index 0, nums: [3, 4, -1, 1]
+# >> nums: [-1, 4, 3, 1]
+# incrementing index, number is either negative or at the right location
+
+# index 1, nums: [-1, 4, 3, 1]
+# >> nums: [-1, 1, 3, 4]
+# >> nums: [1, -1, 3, 4]
+# incrementing index, number is either negative or at the right location
+
+# index 2, nums: [1, -1, 3, 4]
+# incrementing index, number is either negative or at the right location
+
+# index 3, nums: [1, -1, 3, 4]
+# incrementing index, number is either negative or at the right location
+
+# [1, -1, 3, 4]
 # => 2
