f532a5e107efb679a83d393cbc6d36cff9882942
Vlad Milshtein <kartoshka548@gmail.com>
2022-12-14 22:35:25 -0500

    uplift t3 - median finder using two heaps

--- dynamic_programming/median_finder.rb
+++ dynamic_programming/median_finder.rb
@@ -3,51 +3,51 @@
 # For example, for arr = `[1,3,99]`, the median is `3`.
 # For example, for arr = `[2,3]`, the median is `(2 + 3) / 2 = 2.5`.
 
-# [x, x, x]. [x, x, x] <3 that!
 
-# class MedianFinder
-#   def initialize()
-#     @median = 0.0
-#     @list = []
-#   end
+class MedianFinder
+  def initialize
+    @median = 0.0
+    @list = []
+  end
 
-#   def add_num(num)
-#     array = [num] + @list
-#     @list = array.sort # O(n * log n)
-#     if @list.count.even?
-#       index = @list.count / 2
-#       med = @list[index] + @list[index.pred]
-#       @median = med / 2.0
-#     else
-#       index = @list.count / 2
-#       @median = @list[index]
-#     end
-#     nil
-#   end
+  def add_num(num)
+    array = [num] + @list
+    @list = array.sort # O(n * log n)
+    if @list.count.even?
+      index = @list.count / 2
+      med = @list[index] + @list[index.pred]
+      @median = med / 2.0
+    else
+      index = @list.count / 2
+      @median = @list[index]
+    end
+    nil
+  end
 
-#   def find_median
-#     @median
-#   end
-# end
+  def find_median
+    @median
+  end
+end
 
-# median_finder = MedianFinder.new()
-# median_finder.add_num(1)    # arr = [1]
-# median_finder.add_num(2)    # arr = [1, 2]
-# puts median_finder.find_median() # return 1.5 (i.e., (1 + 2) / 2)
-# median_finder.add_num(3)    # arr[1, 2, 3]
-# puts median_finder.find_median() # return 2.0
+median_finder = MedianFinder.new()
+
+median_finder.add_num(1)                # arr = [1]
+median_finder.add_num(2)                # arr = [1, 2]
+puts median_finder.find_median() == 1.5 # (i.e., (1 + 2) / 2.0)
+
+median_finder.add_num(3)                # arr[1, *2*, 3]
+puts median_finder.find_median() == 2
 
 
 # ========
 require 'algorithms'
 
-
-class MedianFinder
-  def initialize()
+class MedianFinderUsingTwoHeaps
+  def initialize
     @median = 0.0
-    # max heap -> [1, 2, *3*]. [*997*, 998, 999] < min heap
-    # [1, 2, 3] [4, 5] --> 2
-    # [1, 2, 2] [3, 4, 5]
+
+    # max heap -> [1, 2, *3*] ... [*997*, 998, 999] < min heap
+    # [1, 2, 3] [4, 5] -> add 2 -> [1, 2, 2] [3, 4, 5]
     @left, @right = Containers::MaxHeap.new, Containers::MinHeap.new
   end
 
@@ -78,11 +78,11 @@ class MedianFinder
   end
 end
 
-median_finder = MedianFinder.new()
-median_finder.add_num(1)    # arr = [1]
-median_finder.add_num(2)    # arr = [1, 2]
-puts median_finder.find_median() # return 1.5 (i.e., (1 + 2) / 2)
-# 
-median_finder.add_num(3)    # arr[1, 2, 3]
-puts median_finder.find_median() # return 2.0
+median_finder = MedianFinderUsingTwoHeaps.new()
+median_finder.add_num(1)                # left = [1]
+median_finder.add_num(2)                # left = [1], right = [2]
+puts median_finder.find_median() == 1.5 # (i.e., (1 + 2) / 2.0)
+ 
+median_finder.add_num(3)                # left = [1], right = [*2*, 3]
+puts median_finder.find_median() == 2
 
