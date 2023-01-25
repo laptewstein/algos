a3f40a17edc2015a2fb8ad069bf91bffe2e149f1
Vlad Milshtein <kartoshka548@gmail.com>
2023-01-25 05:27:42 -0500

    dp / https://leetcode.com/problems/regular-expression-matching and more

---
+++ backtracking/powerset_and_permutations_backrtacking.rb
@@ -0,0 +1,215 @@
+# all possible combinations of elements including the []
+def powerset(list)
+  subset, result = [], [] # subset is a 'shared bus' across stack frames we add and pop out from
+  dfs = lambda do |idx|
+    return result << subset.clone if idx == list.size
+    subset << list[idx]  # decision to include <element at current index>
+    dfs[idx.succ]
+
+    subset.pop           # backtrack: decision NOT to include <element at current index>
+    dfs[idx.succ]        # (return to initial state) i.e [x] => []
+  end
+  dfs[0]
+  result
+end
+
+puts powerset(%q(ABCD).split //).inspect
+# [
+#   ["A", "B", "C", "D"],
+#   ["A", "B", "C"],
+#   ["A", "B", "D"],
+#   ["A", "B"],
+#   ["A", "C", "D"],
+#   ["A", "C"],
+#   ["A", "D"],
+#   ["A"],
+#   ["B", "C", "D"],
+#   ["B", "C"],
+#   ["B", "D"],
+#   ["B"],
+#   ["C", "D"],
+#   ["C"],
+#   ["D"],
+#   []
+# ]
+
+# all element permutations in different order
+def permutations(list)
+  result = []
+  return [list.clone] if list.count == 1
+  list.size.times do
+    elem = list.shift       # pop from left (queue style)
+    perms = permutations(list)
+    result += perms.map { |permutation| permutation << elem }
+    list << elem            # backtrack (append element)
+  end
+  result
+end
+
+puts permutations(%q(ABCD).split //).inspect
+# [
+#   ["D", "C", "B", "A"],
+#   ["C", "D", "B", "A"],
+#   ["B", "D", "C", "A"],
+#   ["D", "B", "C", "A"],
+#   ["C", "B", "D", "A"],
+#   ["B", "C", "D", "A"],
+
+#   ["A", "D", "C", "B"],
+#   ["D", "A", "C", "B"],
+#   ["C", "A", "D", "B"],
+#   ["A", "C", "D", "B"],
+#   ["D", "C", "A", "B"],
+#   ["C", "D", "A", "B"],
+
+#   ["B", "A", "D", "C"],
+#   ["A", "B", "D", "C"],
+#   ["D", "B", "A", "C"],
+#   ["B", "D", "A", "C"],
+#   ["A", "D", "B", "C"],
+#   ["D", "A", "B", "C"],
+
+#   ["C", "B", "A", "D"],
+#   ["B", "C", "A", "D"],
+#   ["A", "C", "B", "D"],
+#   ["C", "A", "B", "D"],
+#   ["B", "A", "C", "D"],
+#   ["A", "B", "C", "D"]
+# ]
+#
+#
+
+# def permutations(list)
+#   result = []
+#   return [list.clone] if list.count == 1
+#   list.size.times do
+#     elem = list.shift       # pop from left (queue style)
+#     puts "~~~ elem" + elem + " list " + list.inspect
+#     perms = permutations(list)
+#     # puts "||| list " + list.inspect + " perms " + perms.inspect + " ((elem: " + elem
+#     result += perms.map { |permutation| permutation << elem }
+#     list << elem            # backtrack (append element)
+#     # puts "=== list " + list.inspect + " perms " + perms.inspect  end
+#   result
+# end
+
+# ~~~ elemA list ["B", "C", "D"]
+# ~~~ elemB list ["C", "D"]
+# ~~~ elemC list ["D"]
+# ||| list ["D"] perms [["D"]]; elem: C
+# === list ["D", "C"] perms [["D", "C"]]
+# ~~~ elemD list ["C"]
+# ||| list ["C"] perms [["C"]]; elem: D
+# === list ["C", "D"] perms [["C", "D"]]
+# ||| list ["C", "D"] perms [["D", "C"], ["C", "D"]]; elem: B
+# === list ["C", "D", "B"] perms [["D", "C", "B"], ["C", "D", "B"]]
+# ~~~ elemC list ["D", "B"]
+# ~~~ elemD list ["B"]
+# ||| list ["B"] perms [["B"]]; elem: D
+# === list ["B", "D"] perms [["B", "D"]]
+# ~~~ elemB list ["D"]
+# ||| list ["D"] perms [["D"]]; elem: B
+# === list ["D", "B"] perms [["D", "B"]]
+# ||| list ["D", "B"] perms [["B", "D"], ["D", "B"]]; elem: C
+# === list ["D", "B", "C"] perms [["B", "D", "C"], ["D", "B", "C"]]
+# ~~~ elemD list ["B", "C"]
+# ~~~ elemB list ["C"]
+# ||| list ["C"] perms [["C"]]; elem: B
+# === list ["C", "B"] perms [["C", "B"]]
+# ~~~ elemC list ["B"]
+# ||| list ["B"] perms [["B"]]; elem: C
+# === list ["B", "C"] perms [["B", "C"]]
+# ||| list ["B", "C"] perms [["C", "B"], ["B", "C"]]; elem: D
+# === list ["B", "C", "D"] perms [["C", "B", "D"], ["B", "C", "D"]]
+# ||| list ["B", "C", "D"] perms [["D", "C", "B"], ["C", "D", "B"], ["B", "D", "C"], ["D", "B", "C"], ["C", "B", "D"], ["B", "C", "D"]]; elem: A
+# === list ["B", "C", "D", "A"] perms [["D", "C", "B", "A"], ["C", "D", "B", "A"], ["B", "D", "C", "A"], ["D", "B", "C", "A"], ["C", "B", "D", "A"], ["B", "C", "D", "A"]]
+# ~~~ elemB list ["C", "D", "A"]
+# ~~~ elemC list ["D", "A"]
+# ~~~ elemD list ["A"]
+# ||| list ["A"] perms [["A"]]; elem: D
+# === list ["A", "D"] perms [["A", "D"]]
+# ~~~ elemA list ["D"]
+# ||| list ["D"] perms [["D"]]; elem: A
+# === list ["D", "A"] perms [["D", "A"]]
+# ||| list ["D", "A"] perms [["A", "D"], ["D", "A"]]; elem: C
+# === list ["D", "A", "C"] perms [["A", "D", "C"], ["D", "A", "C"]]
+# ~~~ elemD list ["A", "C"]
+# ~~~ elemA list ["C"]
+# ||| list ["C"] perms [["C"]]; elem: A
+# === list ["C", "A"] perms [["C", "A"]]
+# ~~~ elemC list ["A"]
+# ||| list ["A"] perms [["A"]]; elem: C
+# === list ["A", "C"] perms [["A", "C"]]
+# ||| list ["A", "C"] perms [["C", "A"], ["A", "C"]]; elem: D
+# === list ["A", "C", "D"] perms [["C", "A", "D"], ["A", "C", "D"]]
+# ~~~ elemA list ["C", "D"]
+# ~~~ elemC list ["D"]
+# ||| list ["D"] perms [["D"]]; elem: C
+# === list ["D", "C"] perms [["D", "C"]]
+# ~~~ elemD list ["C"]
+# ||| list ["C"] perms [["C"]]; elem: D
+# === list ["C", "D"] perms [["C", "D"]]
+# ||| list ["C", "D"] perms [["D", "C"], ["C", "D"]]; elem: A
+# === list ["C", "D", "A"] perms [["D", "C", "A"], ["C", "D", "A"]]
+# ||| list ["C", "D", "A"] perms [["A", "D", "C"], ["D", "A", "C"], ["C", "A", "D"], ["A", "C", "D"], ["D", "C", "A"], ["C", "D", "A"]]; elem: B
+# === list ["C", "D", "A", "B"] perms [["A", "D", "C", "B"], ["D", "A", "C", "B"], ["C", "A", "D", "B"], ["A", "C", "D", "B"], ["D", "C", "A", "B"], ["C", "D", "A", "B"]]
+# ~~~ elemC list ["D", "A", "B"]
+# ~~~ elemD list ["A", "B"]
+# ~~~ elemA list ["B"]
+# ||| list ["B"] perms [["B"]]; elem: A
+# === list ["B", "A"] perms [["B", "A"]]
+# ~~~ elemB list ["A"]
+# ||| list ["A"] perms [["A"]]; elem: B
+# === list ["A", "B"] perms [["A", "B"]]
+# ||| list ["A", "B"] perms [["B", "A"], ["A", "B"]]; elem: D
+# === list ["A", "B", "D"] perms [["B", "A", "D"], ["A", "B", "D"]]
+# ~~~ elemA list ["B", "D"]
+# ~~~ elemB list ["D"]
+# ||| list ["D"] perms [["D"]]; elem: B
+# === list ["D", "B"] perms [["D", "B"]]
+# ~~~ elemD list ["B"]
+# ||| list ["B"] perms [["B"]]; elem: D
+# === list ["B", "D"] perms [["B", "D"]]
+# ||| list ["B", "D"] perms [["D", "B"], ["B", "D"]]; elem: A
+# === list ["B", "D", "A"] perms [["D", "B", "A"], ["B", "D", "A"]]
+# ~~~ elemB list ["D", "A"]
+# ~~~ elemD list ["A"]
+# ||| list ["A"] perms [["A"]]; elem: D
+# === list ["A", "D"] perms [["A", "D"]]
+# ~~~ elemA list ["D"]
+# ||| list ["D"] perms [["D"]]; elem: A
+# === list ["D", "A"] perms [["D", "A"]]
+# ||| list ["D", "A"] perms [["A", "D"], ["D", "A"]]; elem: B
+# === list ["D", "A", "B"] perms [["A", "D", "B"], ["D", "A", "B"]]
+# ||| list ["D", "A", "B"] perms [["B", "A", "D"], ["A", "B", "D"], ["D", "B", "A"], ["B", "D", "A"], ["A", "D", "B"], ["D", "A", "B"]]; elem: C
+# === list ["D", "A", "B", "C"] perms [["B", "A", "D", "C"], ["A", "B", "D", "C"], ["D", "B", "A", "C"], ["B", "D", "A", "C"], ["A", "D", "B", "C"], ["D", "A", "B", "C"]]
+# ~~~ elemD list ["A", "B", "C"]
+# ~~~ elemA list ["B", "C"]
+# ~~~ elemB list ["C"]
+# ||| list ["C"] perms [["C"]]; elem: B
+# === list ["C", "B"] perms [["C", "B"]]
+# ~~~ elemC list ["B"]
+# ||| list ["B"] perms [["B"]]; elem: C
+# === list ["B", "C"] perms [["B", "C"]]
+# ||| list ["B", "C"] perms [["C", "B"], ["B", "C"]]; elem: A
+# === list ["B", "C", "A"] perms [["C", "B", "A"], ["B", "C", "A"]]
+# ~~~ elemB list ["C", "A"]
+# ~~~ elemC list ["A"]
+# ||| list ["A"] perms [["A"]]; elem: C
+# === list ["A", "C"] perms [["A", "C"]]
+# ~~~ elemA list ["C"]
+# ||| list ["C"] perms [["C"]]; elem: A
+# === list ["C", "A"] perms [["C", "A"]]
+# ||| list ["C", "A"] perms [["A", "C"], ["C", "A"]]; elem: B
+# === list ["C", "A", "B"] perms [["A", "C", "B"], ["C", "A", "B"]]
+# ~~~ elemC list ["A", "B"]
+# ~~~ elemA list ["B"]
+# ||| list ["B"] perms [["B"]]; elem: A
+# === list ["B", "A"] perms [["B", "A"]]
+# ~~~ elemB list ["A"]
+# ||| list ["A"] perms [["A"]]; elem: B
+# === list ["A", "B"] perms [["A", "B"]]
+# ||| list ["A", "B"] perms [["B", "A"], ["A", "B"]]; elem: C
+# === list ["A", "B", "C"] perms [["B", "A", "C"], ["A", "B", "C"]]
+# ||| list ["A", "B", "C"] perms [["C", "B", "A"], ["B", "C", "A"], ["A", "C", "B"], ["C", "A", "B"], ["B", "A", "C"], ["A", "B", "C"]]; elem: D
+# === list ["A", "B", "C", "D"] perms [["C", "B", "A", "D"], ["B", "C", "A", "D"], ["A", "C", "B", "D"], ["C", "A", "B", "D"], ["B", "A", "C", "D"], ["A", "B", "C", "D"]]
---
+++ dp/house_robber.rb
@@ -0,0 +1,98 @@
+# https://leetcode.com/problems/house-robber/
+
+# You are a professional robber planning to rob houses along a street.
+# Each house has a certain amount of money stashed,
+# the only constraint stopping you from robbing each of them is that
+# adjacent houses have security systems connected and it will automatically contact the police
+# if two adjacent houses were broken into on the same night.
+#
+# Given an integer array nums representing the amount of money of each house,
+# return the maximum amount of money you can rob tonight without alerting the police.
+
+# Input: nums = [1,2,3,1]
+# Output: 4
+# Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+# Total amount you can rob = 1 + 3 = 4.
+#
+# Input: nums = [2,7,9,3,1]
+# Output: 12
+# Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+# Total amount you can rob = 2 + 9 + 1 = 12.
+#
+# Input: nums = [2,1,1,2]
+# Output: 4
+# Explanation: Rob house 1 (money = 2), rob house 4 (money = 4).
+# Total amount you can rob = 2 + 2 = 4.
+
+
+# # recursive
+def rob(nums)
+  rec = lambda do |idx|
+    return 0 if idx < 0
+    [ rec[idx - 2] + nums[idx], rec[idx - 1] ].max
+  end
+  rec[nums.size.pred]
+end
+
+require 'benchmark'
+Benchmark.bm( 20 ) do |bm|
+  bm.report('recursive') { rob([2, 1, 1, 3, 1]) == 5 }
+  bm.report { rob([3, 1, 2, 5, 4]) == 9 }
+  bm.report { rob([3, 1, 2, 5, 4, 2]) == 10 }
+end
+
+# recursive with memoization
+def rob(nums)
+  memo = Array.new(nums.size.pred, nil)
+  rec = lambda do |idx|
+    return 0 if idx < 0
+    return memo[idx] if memo[idx]
+    memo[idx] = [ rec[idx - 2] + nums[idx], rec[idx - 1] ].max
+    memo[idx]
+  end
+  rec[nums.size.pred]
+end
+
+require 'benchmark'
+Benchmark.bm( 20 ) do |bm|
+  bm.report('recursive with memo') { rob([2, 1, 1, 3, 1]) == 5 }
+  bm.report { rob([3, 1, 2, 5, 4]) == 9 }
+  bm.report { rob([3, 1, 2, 5, 4, 2]) == 10 }
+end
+
+# dynamic programming, one pass
+# O(n) space, O(n) time
+def rob(nums)
+  dp = nums[0..0] + Array.new(nums.size.pred, nil)
+  (1...nums.size).each do |idx|
+    already_stolen = idx > 1 ? dp[idx - 2] : 0
+    previous_max   = dp[idx - 1]
+    dp[idx]        = [
+      already_stolen + nums[idx], # we rob <current> house and add to <already stolen> amount
+      previous_max                # we skip this house and use previous house max
+    ].max
+  end
+  dp.last
+end
+
+require 'benchmark'
+Benchmark.bm( 20 ) do |bm|
+  bm.report('dynamic programming') { rob([2, 1, 1, 3, 1]) == 5 }
+  bm.report { rob([3, 1, 2, 5, 4]) == 9 }
+  bm.report { rob([3, 1, 2, 5, 4, 2]) == 10 }
+end
+
+#                          user     system      total        real
+# recursive              0.000021   0.000007   0.000028 (  0.000022)
+#                        0.000010   0.000004   0.000014 (  0.000013)
+#                        0.000012   0.000003   0.000015 (  0.000015)
+
+#                          user     system      total        real
+# recursive with memo    0.000013   0.000004   0.000017 (  0.000016)
+#                        0.000008   0.000003   0.000011 (  0.000011)
+#                        0.000018   0.000000   0.000018 (  0.000014)
+
+#                          user     system      total        real
+# dynamic programming    0.000019   0.000000   0.000019 (  0.000018)
+#                        0.000010   0.000000   0.000010 (  0.000009)
+#                        0.000008   0.000000   0.000008 (  0.000008)
