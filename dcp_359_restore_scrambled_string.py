1752e7fbbd5236e352c46ced5f7cce99f2385446
Vlad Milshtein <vladmilshtein@gmail.com>
2024-04-01 17:01:58 -0400

    dcp 359 - Alphabetic digit representations jumbled in a string

--- dcp_359_restore_scrambled_string.py
+++ dcp_359_restore_scrambled_string.py
@@ -35,59 +35,6 @@ def generate_num_helper(counter: C[str]) -> C[int]:
     for value, word_counter in enumerate(WORDS):
         temp = maxsize
         for key in word_counter:
-            # checking the number of occurance of current number
-            if counter[key] >= word_counter[key]:
-                temp = min(temp, counter[key] // word_counter[key])
-            else:
-                temp = 0
-                break
-        else:
-            # updating the input counter to remove the current number
-            curr_counter = Counter()
-            for key in word_counter:
-                curr_counter[key] = word_counter[key] * temp
-            counter = counter - curr_counter
-            result[value] = temp
-    return result
-
-
-"""
-Problem:
-
-You are given a string formed by concatenating several words corresponding to the
-integers zero through nine and then anagramming.
-
-For example, the input could be 'niesevehrtfeev', which is an anagram of
-'threefiveseven'. Note that there can be multiple instances of each integer.
-
-Given this string, return the original integers in sorted order. In the example above,
-this would be 357.
-"""
-
-from collections import Counter
-from sys import maxsize
-from typing import Counter as C
-
-WORDS = [
-    Counter("zero"),
-    Counter("one"),
-    Counter("two"),
-    Counter("three"),
-    Counter("four"),
-    Counter("five"),
-    Counter("six"),
-    Counter("seven"),
-    Counter("eight"),
-    Counter("nine"),
-]
-
-
-def generate_num_helper(counter: C[str]) -> C[int]:
-    # runs in O(1) as all the loops run in constant time
-    result = Counter()
-    for value, word_counter in enumerate(WORDS):
-        temp = maxsize
-        for key in word_counter:
             # check number of occurences of current character
             if counter[key] >= word_counter[key]:
                 temp = min(temp, counter[key] // word_counter[key])
